b31b06dd5c3605b521f1abb506748e662a32db2e
Femees <femeesmk@gamil.com>
2020-10-24 02:25:06 -0400

    added db model, routes, views to display data from db

--- config/app.js
+++ config/app.js
@@ -20,6 +20,7 @@ mongoDB.once('open', () => {
 
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
+let contactsRouter = require('../routes/contact');
 
 let app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact-list', contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
