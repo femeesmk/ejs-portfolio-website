db784aa465c63563c7e28c876385daf0e75a03de
Femees <femeesmk@gamil.com>
2020-10-24 16:01:05 -0400

    added deletion conformation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,16 @@
 // IIFE
-(function(){
+(function () {
   function start() {
-    console.log("App Started");
+    console.log('App Started');
+
+    $('#confModel').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget); // Button that triggered the modal
+      var id = button.data('id'); // Extract info from data-* attributes
+      var href = "/contact-list/delete/" + id;
+      console.log(href);
+      var modal = $(this);
+      modal.find('.modal-footer .delete_confirm').attr("href", href);
+    });
   }
-  window.addEventListener("load", start);
+  window.addEventListener('load', start);
 })();
