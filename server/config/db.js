e3ca68300cb3c6e5ff36b2eaca5825011dcf754e
Femees <femeesmk@gamil.com>
2020-10-25 02:42:57 -0400

    connected to remote db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-  "URI" : "mongodb://localhost/ejs_portfolio"
+  "URI" : "mongodb+srv://femees:09QRstIKaDJdG6Xc@ejsbusinesscontacts.kkvpi.mongodb.net/ejs_portfolio?retryWrites=true&w=majority"
 }
